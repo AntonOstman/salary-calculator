c5592bef25e7b440c6bb0bd344e4322d0f9a237f
anton <antos931@student.liu.se>
2022-07-15 09:45:52 +0200

    - Calculation fix - GUI - Calculation use mothly wage

--- main.py
+++ main.py
@@ -1,53 +1,51 @@
 import tkinter as tk
-from salary import Salary
+from tokenize import Double
 
 print("hello")
 
-salary = Salary()
 
 def calculate(work_hours, entries, label : tk.StringVar):
     if not isinstance(work_hours, dict):
         return
     if not isinstance(entries, dict):
         return
-    total_income = 0
+    income = 0
     print("test")
     for entry in entries:
         if not entries[entry].get():
            continue 
         print( entries[entry].get())
-        #total_income += work_hours[entry]*int(entries[entry].get())
+        income += work_hours[entry]*float(entries[entry].get())
 
-    label = str(total_income)
-
-print (salary.base_income)
+    skatt = 0.67
+    label.set(str(int(income * skatt)))
 
 window = tk.Tk()
 
+
 work_hours = {
-    "night": 60,
-    "evening": 40,
-    "overtime": 40, 
-    "day_weekend": 80, 
-    "night_weekend": 100 
+    "month": 23400,
+    "night": 65,
+    "evening": 43.63,
+    "overtime": 42.97, 
+    "day_weekend": 93.63, 
+    "night_weekend": 110.14 
 }
 
 greeting = tk.Label(text="heloo, work")
 
-money = 0
+money = tk.StringVar() 
 i = 0
 entries = {}
 for shift in work_hours:
-    cost = "0"
     label = tk.Label(window, text = shift).grid(row = i)
-    entry = tk.Entry(window, textvariable=cost)
+    entry = tk.Entry(window)
     entry.grid(row = i, column = 1)
     entries[shift] = entry
     i += 1
     if shift == "night_weekend":
         total_income_label = tk.Label(window, textvariable = money).grid(row = i)
         tk.Button(window, text = "quit", command=window.quit).grid(row=i,column=1)
-        tk.Button(window, text = "calculate", command=calculate(work_hours, entries, money)).grid(row=i,column=1)
+        tk.Button(window, text = "calculate", command= lambda: calculate(work_hours, entries, money)).grid(row=i,column=2)
 
-#greeting.pack()
 window.mainloop()
